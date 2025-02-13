601bbd8009d434aa6e5f456e2c5635031724dd8b
luckydawg78 <christianaverill5@gmail.com>
2025-02-13 14:47:07 -0600

    Pushing to CA

--- Main.py
+++ Main.py
@@ -1,8 +1,17 @@
 import tkinter as tk
 from tkinter import filedialog
+import subprocess
+from stegano import lsb
+from PIL import Image
+from PIL.ExifTags import TAGS
+import cv2
+import numpy as np
+
+
+
 
 def upload_file():
-    file_path = filedialog.askopenfilename()
+    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg *.png")])
     if file_path:
         label.config(text=f"File selected: {file_path}")
 
@@ -16,3 +25,25 @@ label = tk.Label(app, text="No file selected")
 label.pack(pady=20)
 
 app.mainloop()
+
+
+
+hidden_message = lsb.reveal('file_path')
+print (hidden_message)
+
+image = image.open('file_path')
+exif_data = image._getexif()
+
+if exif_data:
+    for tag, value in exif_data.items():
+        tag_name = TAGS.get(tag, tag)
+        print(f"{tag_name:25}: {value}")    
+
+with open('file_path', 'wb') as file:
+    data = file.read()
+print(data[-100:])  # Print last 100 bytes
+
+
+
+image = cv2.imread("image.png")
+print(image.shape)  # Analyze pixel values for anomalies
