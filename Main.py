d4bee2044edfcba32e76c0a63534c8fe3db9a3cb
Big-Hoffa <pbordenet03@gmail.com>
2025-02-18 15:06:55 -0600

    GIF Test

--- Main.py
+++ Main.py
@@ -5,13 +5,13 @@ from stegano import lsb
 
 def upload_file():
     global file_path
-    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpeg *.jpg *.png")])
+    file_path = filedialog.askopenfilename(filetypes=[("Images", "*.jpeg *.jpg *.png *.gif")])
     if file_path:
-        label.config(text=f"File selected: {file_path}")
+        label.config(text=f"File selected: {file_path}", fg="black", bg="green")
 
 def embed_message():
     if not file_path:
-        analysis_label.config(text="Please upload a file first.")
+        analysis_label.config(text="Please enter text to hide.", fg="black", bg="red")
         return
     message = message_entry.get()
     if not message:
@@ -24,26 +24,28 @@ def embed_message():
     except Exception as e:
         analysis_label.config(text=f"Error embedding message: {e}")
 
-app = tk.Tk()
-app.title("File Upload and Steganography Embed GUI")
 
-upload_button = tk.Button(app, text="Upload File", command=upload_file)
-upload_button.pack(pady=20)
+window = tk.Tk()
+window.title("Python Steganography")
+window.configure(bg='#525252')
 
-label = tk.Label(app, text="No file selected")
-label.pack(pady=20)
+upload_button = tk.Button(window, text="Upload File", command=upload_file)
+upload_button.pack(pady=10)
 
-message_label = tk.Label(app, text="Enter the message to hide:")
-message_label.pack(pady=10)
+label = tk.Label(window, text="No file selected", fg="black", bg="red")
+label.pack(pady=10)
 
-message_entry = tk.Entry(app, width=50)
-message_entry.pack(pady=10)
+message_label = tk.Label(window, text="Enter the message to hide:", fg="white", bg="#525252")
+message_label.pack(pady=5)
 
-embed_button = tk.Button(app, text="Embed Message", command=embed_message)
+message_entry = tk.Entry(window, width=35)
+message_entry.pack(pady=5)
+
+embed_button = tk.Button(window, text="Embed Message", command=embed_message)
 embed_button.pack(pady=10)
 
-analysis_label = tk.Label(app, text="")
-analysis_label.pack(pady=20)
+analysis_label = tk.Label(window, text="", bg='#525252', fg="red")
+analysis_label.pack(pady=10)
 
-app.mainloop()
+window.mainloop()
 
